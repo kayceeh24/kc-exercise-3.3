3b6a6f1ef4764bd33c932cbb97c716074d88c988
KC Chester Chester <113367258+kayceeh24@users.noreply.github.com>
2022-11-16 15:42:24 +0000

    if changed into elif and bollean quotes

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
+# Function for determining if year is a leap year
 def is_leap_year (yr):
     if yr % 4 == 0: 
         if yr % 100 == 0:
             if yr % 400 == 0:
-                return("True")
+                return (True)
             else:
-                return("False")
+                return (False)
         else:
-            return("True")
+            return (True)
     else:
-        return("False")
+        return (False)
     
 def days_in_month (year, month):
     if month == 1 or month == 3 or month == 5 or  month == 7 or month == 8 or  month == 9 or month == 10:
-        print("31")
-    if month == 4 or month == 6 or month == 9 or month == 11:
+        return("31")
+    elif month == 4 or month == 6 or month == 9 or month == 11:
         print("30")
     if is_leap_year(year) == "True" and month == 2 :
         print("29")
