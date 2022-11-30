eba29b7699d405e9c3e5da09665cbb8c10b6ca25
KC Chester Chester <113367258+kayceeh24@users.noreply.github.com>
2022-11-30 14:13:06 +0000

    added 12 months

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def is_leap_year (yr):
 
 # Function for determining how many days in a month
 def days_in_month (year, month):
-    if month == 1 or month == 3 or month == 5 or  month == 7 or month == 8 or month == 9 or month == 10:
+    if month == 1 or month == 3 or month == 5 or  month == 7 or month == 8  or month == 10 or month == 12:
         return "31"
     elif month == 4 or month == 6 or month == 11:
         return "30"
