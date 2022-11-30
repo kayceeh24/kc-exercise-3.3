d96885d090dd5f61553ae065d528ecf5f182cadd
KC Chester Chester <113367258+kayceeh24@users.noreply.github.com>
2022-11-30 13:54:20 +0000

    changed the month 9

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ def is_leap_year (yr):
 
 # Function for determining how many days in a month
 def days_in_month (year, month):
-    if month == 1 or month == 3 or month == 5 or  month == 7 or month == 8 or  month == 9 or month == 10:
+    if month == 1 or month == 3 or month == 5 or  month == 7 or  month == 9 or month == 10:
         return (31)
-    elif month == 4 or month == 6 or month == 9 or month == 11:
+    elif month == 4 or month == 6 or month == 11:
         return (30)
-    if is_leap_year(year):
+    elif is_leap_year(year):
         return (29)
     else:
         return (28)
