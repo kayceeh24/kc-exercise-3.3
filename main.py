ee89612e49bd028adbb06901651e7597609ae52b
KC Chester Chester <113367258+kayceeh24@users.noreply.github.com>
2022-11-30 14:14:57 +0000

    added 9 months

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def is_leap_year (yr):
 def days_in_month (year, month):
     if month == 1 or month == 3 or month == 5 or  month == 7 or month == 8  or month == 10 or month == 12:
         return "31"
-    elif month == 4 or month == 6 or month == 11:
+    elif month == 4 or month == 6 or month == 9 or month == 11:
         return "30"
     elif is_leap_year(year):
         return "29"
