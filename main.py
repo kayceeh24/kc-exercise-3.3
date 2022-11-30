c7603ad537a4859ac919560171961f9677e2b9da
KC Chester Chester <113367258+kayceeh24@users.noreply.github.com>
2022-11-30 03:47:23 +0000

    parenthesis

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ def is_leap_year (yr):
 # Function for determining how many days in a month
 def days_in_month (year, month):
     if month == 1 or month == 3 or month == 5 or  month == 7 or month == 8 or  month == 9 or month == 10:
-        return "31"
+        return 31
     elif month == 4 or month == 6 or month == 9 or month == 11:
-        return "30"
+        return 30
     elif is_leap_year(year):
-        return "29"
+        return 29
     else:
-        return "28"
+        return 28
     
